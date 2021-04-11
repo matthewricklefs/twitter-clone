fd4c02f847944682f64c952a0f14e60e4c3b4e44
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-04-11 16:27:06 -0400

    adds the SWR dependency and then begins fetching mockdata to produce dynamic information inside of the body to replicate individual tweets

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,28 @@
+import useSWR from 'swr'
+
+const fetcher = (url) => fetch(url).then((res) => res.json())
+
 function Home() {
-  return <p>Hello!</p>;
+  let { data } = useSWR('/api/tweets', fetcher)
+
+  if (!data) return "Loading..."
+
+  return (
+    <div>
+      {[...data.tweets].reverse().map((tweet) => (
+        <div className="px-4 py-3 border-b border-gray-200">
+          <div className="flex">
+            <img 
+              className="w-10 h-10 mr-3 rounded-full"
+              src={tweet.avatarUrl}
+              alt=""
+            />
+            <p className="text-sm">{tweet.text}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 Home.headerTitle = "Latest Tweets";
